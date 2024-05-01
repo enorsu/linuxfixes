177c2b8899e0ec214e7c075be12f80e295ed8c41
SotaNorsu <105721278+enorsu@users.noreply.github.com>
2024-05-01 07:14:50 +0000

    Update flatpak-fix-for-plasma-debian.sh

--- flatpak-fix-for-plasma-debian.sh
+++ flatpak-fix-for-plasma-debian.sh
@@ -1,1 +1,12 @@
 
+# Tested on Debian 12(Bookworm) with KDE Plasma desktop
+# What it does?
+# It fixes the cursor theme in flatpak applications
+# How to use?
+# You need Flatpak installed
+# Then run this script
+# You can also copy and paste the script below
+# Does not need root
+
+flatpak --user override --filesystem=/home/$USER/.icons/:ro
+flatpak --user override --filesystem=/usr/share/icons/:ro
